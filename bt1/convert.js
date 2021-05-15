d724b95544abaa5c4e598d6fcf52119bda9fb145
huyhd96 <huychelsea0@gmail.com>
2021-05-15 07:33:32 -0400

    first commit

---
+++ bt1/convert.js
@@ -0,0 +1,3 @@
+let a = prompt("nhập độ C:")
+let b = 9 / 5 * a + 32
+alert("Độ F= " + b)
---
+++ bt2/caculator.js
@@ -0,0 +1,5 @@
+let a = +prompt("nhập điểm môn Văn:")
+let b = +prompt("Nhập điểm môn Toán:")
+let c = +prompt("Nhập điểm môn Anh:")
+let diemtb = (a + b + c) / 3
+alert("Điểm trung bình 3 môn là:" + diemtb)
